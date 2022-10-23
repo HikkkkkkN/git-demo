d3dab13bdbc798969efa578cb00edb0d1dbeecfa
hikn <hikn@ggg.com>
2022-10-23 08:47:33 +0800

    commit dev 1

--- src/main/java/com/itheima/GitDemo.java
+++ src/main/java/com/itheima/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println("github提交");
+        System.out.println("dev提交");
     }
 }
