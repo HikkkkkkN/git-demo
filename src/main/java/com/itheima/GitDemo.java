195df742e86cebd454c4f26c21fccf15f5559108
HikkkkkkN <107463349+HikkkkkkN@users.noreply.github.com>
2022-10-23 08:53:02 +0800

    Update GitDemo.java

--- src/main/java/com/itheima/GitDemo.java
+++ src/main/java/com/itheima/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
         System.out.println(1);
         System.out.println("github提交");
         System.out.println("dev提交");
+        System.out.println(" github dev提交");
     }
 }
